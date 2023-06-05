e62e6f33c3e83feac8a32e8926265899b67b2347
Jakob Sjöholm <jakob.sjoholm@live.se>
2023-06-05 13:29:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { changeBackgroundColor } from '@kraktass/background-color-changer';
+import transformToUpperCase from '@kraktass/text-transformer';
 
 function App() {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const calculate = () => {
+    const intA = parseInt(a);
+    const intB = parseInt(b);
+    console.log(`${intA + intB}`);
+  };
+
+  const changeBackgroundColorHandler = () => {
+    changeBackgroundColor('red');
+  };
+
+  const text = "This text is made uppercase from an NPM package";
+  const transformedText = transformToUpperCase(text);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{transformedText}</h1>
+      <p>First number:</p>
+      <input type="number" value={a} onChange={e => setA(e.target.value)} />
+      <p>Second number:</p>
+      <input type="number" value={b} onChange={e => setB(e.target.value)} />
+      <p>
+        <button onClick={calculate}>Calculate</button>
+        <button id="colorChanger" onClick={changeBackgroundColorHandler}>
+          Change background color
+        </button>
+      </p>
     </div>
   );
 }
